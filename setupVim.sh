bb481410f6ede908d3bf9781e8116ec530aefd1e
Kien Weng <mycoder80@gmail.com>
2013-07-10 10:26:42 +0800

    added setup*.sh

---
+++ setupVim.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir -p ~/.vim/autoload ~/.vim/bundle; 
+
+# Install pathogen
+curl -Sso ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+
+# Install Tabular
+mkdir -p ~/.vim/bundle
+cd ~/.vim/bundle
+git clone git://github.com/godlygeek/tabular.git
+
+# Install jshint
+mkdir -p ~/.vim/bundle
+cd ~/.vim/bundle
+git clone https://github.com/walm/jshint.vim
+
